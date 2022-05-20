d3b658362876ff1800f7743cf18052e880e75a28
papojari <papojari-git.ovoid@aleeas.com>
2022-05-21 00:34:56 +0200

    fix: remove commented code in stopwatch script

--- ui/stopwatch/default.gd
+++ ui/stopwatch/default.gd
@@ -12,17 +12,3 @@ func _process(delta):
 	var hours = fmod(fmod(global.robert_time, 3600 * 24) / 3600, 24)
 
 	label.text = "%02d:%02d:%02d.%03d" % [hours, mins, secs, milsecs]
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	if !global.robert_finished:
-#		get_time()
-#	
-#func get_time():
-#	var elapsed_milliseconds = OS.get_ticks_msec()
-#	var milliseconds = elapsed_milliseconds % 1000
-#	var seconds = elapsed_milliseconds / 1000 % 60
-#	var minutes = elapsed_milliseconds / 60000 % 60
-#	var elapsed_hours = elapsed_milliseconds / 3600000
-#	var elapsed_time = "%02d:%02d:%02d:%03d" % [elapsed_hours, minutes, seconds, milliseconds]
-#	label.text = elapsed_time
